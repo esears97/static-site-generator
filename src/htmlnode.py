866ee14edb89756a5f47b8a8c3de252d1566caed
Eric <esears97@gmail.com>
2025-05-30 17:03:28 -0400

    text node to html node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -28,3 +28,16 @@ class LeafNode(HTMLNode):
             return f"{self.value}"
         else:
           return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props = {}):
+        super().__init__(tag, None, children, props = {})
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("tag cannot be None")
+        if self.children is None:
+            raise ValueError("missing children")
+        result = ""
+        for child in self.children:
+            result += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{result}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props(self):
@@ -23,5 +23,18 @@ class TestHTMLNode(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             node.to_html()
         self.assertEqual(str(context.exception), "value cannot be None")
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
 if __name__ == "__main__":
     unittest.main()
